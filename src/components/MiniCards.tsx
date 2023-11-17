97f914eac26c8cb6daf7dd2d1b15175f2c13c110
Santiago Sierra Cano <santiscano@gmail.com>
2023-11-17 07:29:39 -0500

    feat:styles minicards

--- src/components/MiniCards.tsx
+++ src/components/MiniCards.tsx
@@ -1,10 +1,26 @@
+import '../styles/miniCards.css';
 
 const MiniCards = () => {
+  const arrCards = [
+    {icon: "../assets/svg/arrow.svg"},
+    {icon: "../assets/svg/arrow.svg"},
+    {icon: "../assets/svg/arrow.svg"},
+  ];
+
   return (
     <div className="container__mini-cards">
-      <p>hola</p>
-      <p>hola</p>
-      <p>hola</p>
+      {arrCards.map(({icon}, index) => (
+        <div
+          key={index}
+          className="container-card"
+        >
+          <img src={icon} alt="icono" />
+          <div className="texts">
+            <h3>titulo</h3>
+            <p>descripcion bien larga</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
